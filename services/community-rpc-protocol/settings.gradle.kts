999fda9b7a8596c5b6355c9bbfb53ac2659ca330
Dhruv Singhal <dhruvsinghal@Dhruvs-MacBook-Pro.local>
2022-10-09 16:50:14 +0530

    Added grpc protos and apis for community endpoints

---
+++ services/community-rpc-protocol/settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "community-rpc-protocol"
+include("community-rpc-api")
+include("community-rpc-impl")
+include("community-rpc-service")
+include("community-rpc-service-factory")
+include("community-rpc-service-impl")
+include("community-api")
