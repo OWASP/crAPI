cced025fa0c655f30b3d8199f4865e1604782b03
Nikhil R <nikhil@traceable.ai>
2022-11-04 13:57:07 +0530

    Updated identity service to use user provided private key

--- services/identity/entrypoint.sh
+++ services/identity/entrypoint.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 set -e
 
-JWT_PRIVATE_KEY=$(openssl rsa -in /.keys/private.pem -outform der | openssl base64 -A)
-JWT_PUBLIC_KEY=$(openssl rsa -in /.keys/private.pem -RSAPublicKey_out -outform DER | openssl base64 -A)
+JWT_PRIVATE_KEY=$(openssl pkcs8 -topk8 -in /.keys/private.pem -outform der -nocrypt | openssl base64 -A)
+JWT_PUBLIC_KEY=$(openssl pkey -in /.keys/private.pem -pubout -outform der | openssl base64 -A)
 
-echo "Private Key: ${JWT_PRIVATE_KEY}"
-echo "Public Key: ${JWT_PUBLIC_KEY}"
-
-java -jar /app/identity-service-1.0-SNAPSHOT.jar
+java -jar /app/identity-service-1.0-SNAPSHOT.jar --app.jwtPrivateKey=$JWT_PRIVATE_KEY --app.jwtPublicKey=$JWT_PUBLIC_KEY
 
 exec "$@"
--- services/identity/src/main/java/com/crapi/config/JwtProvider.java
+++ services/identity/src/main/java/com/crapi/config/JwtProvider.java
@@ -16,8 +16,15 @@ package com.crapi.config;
 
 import com.crapi.entity.User;
 import io.jsonwebtoken.*;
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,11 +36,31 @@ public class JwtProvider {
 
   private static final Logger logger = LoggerFactory.getLogger(JwtProvider.class);
 
-  @Value("${app.jwtSecret}")
-  private String jwtSecret;
-
   @Value("${app.jwtExpiration}")
   private int jwtExpiration;
+
+  private KeyPair keyPair;
+
+  public JwtProvider(
+      @Value("${app.jwtPrivateKey}") String privateKey,
+      @Value("${app.jwtPublicKey}") String publicKey) {
+    try {
+      Base64.Decoder decoder = Base64.getDecoder();
+      byte[] publicBytes = decoder.decode(publicKey);
+      byte[] privateBytes = decoder.decode(privateKey);
+
+      X509EncodedKeySpec X509publicKey = new X509EncodedKeySpec(publicBytes);
+      PKCS8EncodedKeySpec PKCS8privateKey = new PKCS8EncodedKeySpec(privateBytes);
+
+      KeyFactory kf = KeyFactory.getInstance("RSA");
+      RSAPublicKey rsaPubKey = (RSAPublicKey) kf.generatePublic(X509publicKey);
+      RSAPrivateKey rsaPrivKey = (RSAPrivateKey) kf.generatePrivate(PKCS8privateKey);
+
+      this.keyPair = new KeyPair(rsaPubKey, rsaPrivKey);
+    } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+      throw new RuntimeException(e);
+    }
+  }
 
   /**
    * @param user
@@ -45,7 +72,7 @@ public class JwtProvider {
         .claim("role", user.getRole().getName())
         .setIssuedAt(new Date())
         .setExpiration(new Date((new Date()).getTime() + jwtExpiration))
-        .signWith(SignatureAlgorithm.HS512, jwtSecret.getBytes(StandardCharsets.UTF_8))
+        .signWith(SignatureAlgorithm.RS256, this.keyPair.getPrivate())
         .compact();
   }
 
@@ -55,7 +82,7 @@ public class JwtProvider {
    */
   public String getUserNameFromJwtToken(String token) {
     return Jwts.parser()
-        .setSigningKey(jwtSecret.getBytes(StandardCharsets.UTF_8))
+        .setSigningKey(this.keyPair.getPublic())
         .parseClaimsJws(token)
         .getBody()
         .getSubject();
@@ -67,7 +94,7 @@ public class JwtProvider {
    */
   public boolean validateJwtToken(String authToken) {
     try {
-      Jwts.parser().setSigningKey(jwtSecret.getBytes("utf-8")).parseClaimsJws(authToken);
+      Jwts.parser().setSigningKey(this.keyPair.getPublic()).parseClaimsJws(authToken);
       return true;
     } catch (SignatureException e) {
       logger.error("Invalid JWT signature -> Message: %d ", e);
@@ -79,8 +106,8 @@ public class JwtProvider {
       logger.error("Unsupported JWT token -> Message: %d", e);
     } catch (IllegalArgumentException e) {
       logger.error("JWT claims string is empty -> Message: %d", e);
-    } catch (UnsupportedEncodingException e) {
-      logger.error("Unable to convert into byte -> Message: %d", e);
+      //    } catch (UnsupportedEncodingException e) {
+      //      logger.error("Unable to convert into byte -> Message: %d", e);
     }
 
     return false;
