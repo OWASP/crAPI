1b338594a6876062d3f97ad4a2929c39c0f4f3f3
Nikhil R <nikhil@traceable.ai>
2022-11-18 18:32:10 +0530

    Fixed jwks bug (#158)

--- services/identity/entrypoint.sh
+++ services/identity/entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 set -e
 
-JWKS=$(openssl base64 -in /.keys/jwks.json -A)
+if [ -f /keys/jwks.json ]; then
+  JWKS=$(openssl base64 -in /keys/jwks.json -A)
+else
+  echo "Loading default JWKS file."
+  JWKS=$(openssl base64 -in /default_jwks.json -A)
+fi
+
 java -jar /app/identity-service-1.0-SNAPSHOT.jar --app.jwksJson=$JWKS
 
 exec "$@"
