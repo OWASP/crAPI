4a6988b095c6637705e4f0947b45820ef68e7ee2
Roshan Piyush <roshan.piyush@traceable.ai>
2023-03-29 16:09:05 +0530

    Tls external (#183)
    * Fetch paymentinfo/vehicleowners from external api gateway

---
+++ services/workshop/utils/helper.py
@@ -0,0 +1,6 @@
+from base64 import b64encode
+
+
+def basic_auth(username, password=""):
+    token = b64encode(f"{username}:{password}".encode('utf-8')).decode("ascii")
+    return f'Basic {token}'
